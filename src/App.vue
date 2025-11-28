a268cc99aa939ede0de7daf288aea26923f28763
Dimitar Dimov <dimitar.dimov2000@gmail.com>
2025-11-28 09:47:56 +0100

    Added /drinks fetchpoint for clear json result

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,37 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue'
+
+interface Drink {
+  id: number
+  name: string
+  price: number
+}
 
 const header = ref('KPI Dashboard App')
-const items = ref([
-  {id: 1, label: "5 Martinis"},
-  {id: 2, label: "2 Aperol Spritz"},
-  {id: 3, label: "20 Beers"}
-])
+const items = ref<Drink[]>([])
+
+function loadThings() {
+  const endpoint = 'https://kpi-dashboard-wfqf.onrender.com/drinks'
+
+  fetch(endpoint)
+    .then(res => res.json())
+    .then((data: Drink[]) => {
+      items.value = data
+    })
+    .catch(err => console.error(err))
+}
+
+onMounted(() => {
+  loadThings()
+})
 </script>
 
 <template>
   <h1>{{ header }}</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
 
   <ul>
-    <li v-for="item in items" :key="item.id">{{item.label}}</li>
+    <li v-for="item in items" :key="item.id">
+      {{ item.name }} – {{ item.price }} €
+    </li>
   </ul>
 </template>
-
-<style scoped></style>
